9ce17ccb149c795872c9823fb8a9e4f5219a0a9f
pyrhaa <sboubtane@protonmail.com>
2021-08-20 10:04:43 +0200

    reviews hook ok

--- src/hooks/useReviewsContract.js
+++ src/hooks/useReviewsContract.js
@@ -1,72 +1,47 @@
-import { useContext, useEffect, useState } from "react"
-import { Web3Context } from "web3-hooks"
-import { ReviewsContext } from "../contexts/ReviewsContext"
+import { useContext, useEffect, useState } from 'react';
+import { Web3Context } from 'web3-hooks';
+import { ReviewsContext } from '../contexts/ReviewsContext';
 
 const getReviewsData = async (reviews, id) => {
   const r = await reviews.reviewInfo(id);
-}
 
-const reviewObj = {}
+  const reviewObj = {
+    id: r.id.toNumber(),
+    author: r.author,
+    contentCID: r.contentCID,
+    contentBanned: r.contentBanned,
+    targetID: r.targetID.toNumber(),
+    comments: r.comments
+  };
+  return reviewObj;
+};
 
-const useReviewsContract = () => {
-  const [reviews] = useContext(ReviewsContext)
+export const useReviewsContract = () => {
+  const [reviews] = useContext(ReviewsContext);
+  const [web3State] = useContext(Web3Context);
+
+  const [reviewList, setReviewList] = useState([]);
+
+  useEffect(() => {
+    if (reviews) {
+      const createReviewList = async () => {
+        const nb = await reviews.nbOfReviews();
+        const reviewsList = [];
+        for (let i = 1; i <= nb; i++) {
+          const obj = await getReviewsData(reviews, i);
+          reviewsList.push(obj);
+        }
+        setReviewList(reviewsList);
+      };
+      createReviewList();
+    }
+  }, [reviews]);
 
   // control call of the hook
   if (reviews === undefined) {
     throw new Error(
-      `It seems that you are trying to use ArticlesContext outside of its provider`
-    )
+      `It seems that you are trying to use ReviewsContext outside of its provider`
+    );
   }
-  return reviews
-}
-
-const getArticleData = async (articles, id) => {
-  const a = await articles.articleInfo(id)
-
-  const articleObj = {
-    id: a.id.toNumber(),
-    author: a.author,
-    coAuthor: a.coAuthor,
-    contentBanned: a.contentBanned,
-    abstractCID: a.abstractCID,
-    contentCID: a.contentCID,
-    comments: a.comments,
-    reviews: a.reviews,
-  }
-  return articleObj
-}
-
-// hooks
-export const useArticlesContract = () => {
-  // call the context
-  const [articles] = useContext(ArticlesContext)
-
-  // utils
-  const [web3State] = useContext(Web3Context)
-  const [articleList, setArticleList] = useState([])
-
-  useEffect(() => {
-    if (articles) {
-      const createArticleList = async () => {
-        const nb = await articles.nbOfArticles()
-        const articlesList = []
-        for (let i = 1; i <= nb; i++) {
-          const obj = await getArticleData(articles, i)
-          articlesList.push(obj)
-        }
-        setArticleList(articlesList)
-      }
-      createArticleList()
-    }
-  }, [articles])
-
-  // control call of the hook
-  if (articles === undefined) {
-    throw new Error(
-      `It seems that you are trying to use ArticlesContext outside of its provider`
-    )
-  }
-
-  // first: return contract for utilisation
-  return [articles, articleList, getArticleData]
-}
+  return [reviews, reviewList, getReviewsData];
+};
