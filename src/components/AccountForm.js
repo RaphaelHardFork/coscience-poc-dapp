fe57367dfffad72c5dcd61d855e9f24fe42d7f4d
pyrhaa <sboubtane@protonmail.com>
2021-09-10 15:25:21 +0200

    sign up problem in progress

--- src/components/AccountForm.js
+++ src/components/AccountForm.js
@@ -8,156 +8,156 @@ import {
   useColorModeValue,
   Textarea,
   SlideFade,
-  Text,
-} from "@chakra-ui/react"
-import { ethers } from "ethers"
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import { useUsersContract } from "../hooks/useUsersContract"
-import { useIPFS } from "../hooks/useIPFS"
-import { useCall } from "../web3hook/useCall"
+  Text
+} from '@chakra-ui/react';
+import { ethers } from 'ethers';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useUsersContract } from '../hooks/useUsersContract';
+import { useIPFS } from '../hooks/useIPFS';
+import { useCall } from '../web3hook/useCall';
 
 const AccountForm = () => {
-  const [users] = useUsersContract() // [contract]
-  const [status, contractCall] = useCall()
-  const [pinJsObject, , ipfsStatus] = useIPFS()
+  const { users } = useUsersContract(); // [contract]
+  const [status, contractCall] = useCall();
+  const [pinJsObject, , ipfsStatus] = useIPFS();
 
   // factorize in a reducer? with initial state
-  const [firstName, setFirstName] = useState("")
-  const [lastName, setLastName] = useState("")
-  const [laboratory, setLaboratory] = useState("")
-  const [email, setEmail] = useState("")
-  const [registerMail, setRegisterMail] = useState("")
-  const [bio, setBio] = useState("")
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [laboratory, setLaboratory] = useState('');
+  const [email, setEmail] = useState('');
+  const [registerMail, setRegisterMail] = useState('');
+  const [bio, setBio] = useState('');
 
   //color Mode
-  const bg = useColorModeValue("white", "gray.800")
-  const scheme = useColorModeValue("colorMain", "colorSecond")
+  const bg = useColorModeValue('white', 'gray.800');
+  const scheme = useColorModeValue('colorMain', 'colorSecond');
 
   async function register() {
     const nameObj = {
       version: 0.1,
       firstName,
       lastName,
-      registerMail,
-    }
-    const nameHash = await pinJsObject(nameObj)
+      registerMail
+    };
+    const nameHash = await pinJsObject(nameObj);
     const userObj = {
       version: 0.1,
       userInfo: nameHash,
       email,
       laboratory,
-      bio,
-    }
-    const userHash = await pinJsObject(userObj)
-    await contractCall(users, "register", [userHash, nameHash])
-    setBio("")
-    setLaboratory("")
-    setLastName("")
-    setFirstName("")
-    setEmail("")
-    setRegisterMail("")
+      bio
+    };
+    const userHash = await pinJsObject(userObj);
+    await contractCall(users, 'register', [userHash, nameHash]);
+    setBio('');
+    setLaboratory('');
+    setLastName('');
+    setFirstName('');
+    setEmail('');
+    setRegisterMail('');
   }
 
   return (
     <>
-      <Box borderRadius="50" bg={bg} py="10">
+      <Box borderRadius='50' bg={bg} py='10'>
         <SlideFade
-          threshold="0.1"
+          threshold='0.1'
           delay={{ enter: 0.1 }}
           transition={{
-            enter: { duration: 0.7 },
+            enter: { duration: 0.7 }
           }}
-          offsetY="-100px"
-          offsetX="0px"
+          offsetY='-100px'
+          offsetX='0px'
           in
         >
-          <Heading textAlign="center" mb="2">
+          <Heading textAlign='center' mb='2'>
             Create an account to publish an article
           </Heading>
-          <Box mx="auto" maxW="50%" display="flex" flexDirection="column">
-            <Heading mt="4" fontSize="xl" as="h3">
+          <Box mx='auto' maxW='50%' display='flex' flexDirection='column'>
+            <Heading mt='4' fontSize='xl' as='h3'>
               Essential informations
             </Heading>
-            <Text mb="4" color="gray">
+            <Text mb='4' color='gray'>
               These informations will be stored permanently and can't be
               editable.
             </Text>
-            <FormControl mb="4">
+            <FormControl mb='4'>
               <FormLabel>First name</FormLabel>
               <Input
                 onChange={(e) => setFirstName(e.target.value)}
-                placeholder="Alice"
+                placeholder='Alice'
                 value={firstName}
               />
             </FormControl>
-            <FormControl mb="4">
+            <FormControl mb='4'>
               <FormLabel>Last name</FormLabel>
               <Input
                 onChange={(e) => setLastName(e.target.value)}
-                placeholder="Bob"
+                placeholder='Bob'
                 value={lastName}
               />
             </FormControl>
-            <FormControl mb="4">
+            <FormControl mb='4'>
               <FormLabel>Institutional e-mail for registration</FormLabel>
               <Input
-                type="email"
+                type='email'
                 onChange={(e) => setRegisterMail(e.target.value)}
-                placeholder="Bob@alice.com"
+                placeholder='Bob@alice.com'
                 value={registerMail}
               />
             </FormControl>
-            <Heading mt="4" fontSize="xl" as="h3">
+            <Heading mt='4' fontSize='xl' as='h3'>
               About you
             </Heading>
-            <Text mb="4" color="gray">
+            <Text mb='4' color='gray'>
               Except the laboratory, these informations are not essential to
               register and can be editable in your profile.
             </Text>
-            <FormControl mb="4">
+            <FormControl mb='4'>
               <FormLabel>Laboratory</FormLabel>
               <Input
                 onChange={(e) => setLaboratory(e.target.value)}
-                placeholder="MIT"
+                placeholder='MIT'
                 value={laboratory}
               />
             </FormControl>
-            <FormControl mb="4">
+            <FormControl mb='4'>
               <FormLabel>Your contact e-mail</FormLabel>
               <Input
-                type="email"
+                type='email'
                 onChange={(e) => setEmail(e.target.value)}
-                placeholder="Bob@alice.com"
+                placeholder='Bob@alice.com'
                 value={email}
               />
             </FormControl>
-            <FormControl mb="4">
+            <FormControl mb='4'>
               <FormLabel>Bio</FormLabel>
               <Textarea
-                minH="40"
+                minH='40'
                 onChange={(e) => setBio(e.target.value)}
-                placeholder="Your experience, ..."
+                placeholder='Your experience, ...'
                 value={bio}
               />
             </FormControl>
             <Button
               isLoading={
-                status.startsWith("Waiting") ||
-                status.startsWith("Pending") ||
-                ipfsStatus.startsWith("Pinning")
+                status.startsWith('Waiting') ||
+                status.startsWith('Pending') ||
+                ipfsStatus.startsWith('Pinning')
               }
               loadingText={
-                ipfsStatus.startsWith("Pinning") ? ipfsStatus : status
+                ipfsStatus.startsWith('Pinning') ? ipfsStatus : status
               }
               disabled={
                 !firstName.length ||
                 !lastName.length ||
                 !laboratory.length ||
                 !registerMail.length ||
-                status.startsWith("Waiting") ||
-                status.startsWith("Pending") ||
-                ipfsStatus.startsWith("Pinning")
+                status.startsWith('Waiting') ||
+                status.startsWith('Pending') ||
+                ipfsStatus.startsWith('Pinning')
               }
               onClick={register}
               colorScheme={scheme}
@@ -168,27 +168,27 @@ const AccountForm = () => {
         </SlideFade>
 
         <SlideFade
-          threshold="0.1"
+          threshold='0.1'
           delay={{ enter: 0.1 }}
           transition={{
-            enter: { duration: 0.7 },
+            enter: { duration: 0.7 }
           }}
-          offsetY="100px"
-          offsetX="0px"
+          offsetY='100px'
+          offsetX='0px'
           in
         >
-          <Heading mt="6" textAlign="center" mb="2">
+          <Heading mt='6' textAlign='center' mb='2'>
             Or recover your profile with your password
           </Heading>
-          <Box mx="auto" maxW="50%" display="flex" flexDirection="column">
-            <Button mt="6" as={Link} to="/recover" colorScheme="messenger">
+          <Box mx='auto' maxW='50%' display='flex' flexDirection='column'>
+            <Button mt='6' as={Link} to='/recover' colorScheme='messenger'>
               Recover account
             </Button>
           </Box>
         </SlideFade>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default AccountForm
+export default AccountForm;
